33dd1e6fc829b2d7980be9e263be4b46f671cbd1
DESKTOP-JQ56L0P\skfdk <skfdkfkalsrn@gmail.com>
2021-03-24 15:01:03 +0900

    lombok 사용하기

--- Chapter01/src/main/java/com/ruby/persistence/jdbc/EmployeeVO.java
+++ Chapter01/src/main/java/com/ruby/persistence/jdbc/EmployeeVO.java
@@ -2,6 +2,9 @@ package com.ruby.persistence.jdbc;
 
 import java.sql.Timestamp;
 
+import lombok.Data;
+
+@Data
 public class EmployeeVO
 {
 	private Long id;
@@ -9,62 +12,6 @@ public class EmployeeVO
 	private Timestamp startDate;
 	private String title;
 	private String deptName;
-	private Double salary;
-	public Long getId()
-	{
-		return id;
-	}
-	public void setId(Long id)
-	{
-		this.id = id;
-	}
-	public String getName()
-	{
-		return name;
-	}
-	public void setName(String name)
-	{
-		this.name = name;
-	}
-	public Timestamp getStartDate()
-	{
-		return startDate;
-	}
-	public void setStartDate(Timestamp startDate)
-	{
-		this.startDate = startDate;
-	}
-	public String getTitle()
-	{
-		return title;
-	}
-	public void setTitle(String title)
-	{
-		this.title = title;
-	}
-	public String getDeptName()
-	{
-		return deptName;
-	}
-	public void setDeptName(String deptName)
-	{
-		this.deptName = deptName;
-	}
-	public Double getSalary()
-	{
-		return salary;
-	}
-	public void setSalary(Double salary)
-	{
-		this.salary = salary;
-	}
-	
-	@Override
-	public String toString()
-	{
-		return "EmployeeVO [id=" + id + ", name=" + name +
-				", startDate=" + startDate + ", title=" + title +
-				", deptName=" + deptName + ", salary=" + salary + "]";
-	}
+	private Double salary;	
 	
 }
